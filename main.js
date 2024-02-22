67ef6767e36781e25f1c19c0b81d483e821c3913
Zach Wolek <zach.wolek@gmail.com>
2024-02-21 20:32:47 -0600

    Complete Iteration 2

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ var lock4 = document.querySelector('#lock4');
 var lock5 = document.querySelector('#lock5');
 
 var boxes = [
-    {box: document.querySelector('#box1'), label: document.querySelector('#label1')},
-    {box: document.querySelector('#box2'), label: document.querySelector('#label2')},
-    {box: document.querySelector('#box3'), label: document.querySelector('#label3')},
-    {box: document.querySelector('#box4'), label: document.querySelector('#label4')},
-    {box: document.querySelector('#box5'), label: document.querySelector('#label5')}
+    {box: document.querySelector('#box1'), label: document.querySelector('#label1'), locked: false},
+    {box: document.querySelector('#box2'), label: document.querySelector('#label2'), locked: false},
+    {box: document.querySelector('#box3'), label: document.querySelector('#label3'), locked: false},
+    {box: document.querySelector('#box4'), label: document.querySelector('#label4'), locked: false},
+    {box: document.querySelector('#box5'), label: document.querySelector('#label5'), locked: false}
 ];
 
 var currentPalette = [];
+var lockedColors = [];
 
 addEventListener("load", showRandomColors);
 
@@ -23,49 +24,54 @@ newPalette.addEventListener("click", function(){
 
 function showRandomColors() {
     currentPalette = [];
-    boxes.forEach(function(item) {
-        if (!item.locked){
-        var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    boxes.forEach(function(item, index) {
+        var randomColor = null
+        if (!lockedColors[index]){
+            var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        }
+        else {
+            randomColor = lockedColors[index]
+        }
         item.box.style.backgroundColor = randomColor;
         item.label.innerText = randomColor;
         currentPalette.push(randomColor);
-        }
     });
 };
 
 
 lock1.addEventListener('click', function() {
   console.log('here');
-  handleLock(lock1);
+  handleLockIcon(lock1, 0);
 });
 
 lock2.addEventListener('click', function() {
   console.log('here');
-  handleLock(lock2);
+  handleLockIcon(lock2, 1);
 });
 
 lock3.addEventListener('click', function() {
     console.log('here');
-    handleLock(lock3);
+    handleLockIcon(lock3, 2);
 });
 
 lock4.addEventListener('click', function() {
     console.log('here');
-    handleLock(lock4);
+    handleLockIcon(lock4, 3);
 });
 
 lock5.addEventListener('click', function() {
     console.log('here');
-    handleLock(lock5);
+    handleLockIcon(lock5, 4);
 });
 
 
-function handleLock(lock) {
+function handleLockIcon(lock, index) {
   var isLocked = lock.src.includes("unlocked");
   if (isLocked) {
-      lock.src = "assets/locked.png";
+        lockedColors[index] = boxes[index].label.innerText
+        lock.src = "assets/locked.png";    
   } else {
+        lockedColors[index] = null
       lock.src = "assets/unlocked.png";
   }
 };
-
