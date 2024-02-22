c5fa87d58065131e825ec54820fb1ee778c3e752
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-02-21 18:10:23 -0700

    Allow lock icons to toggle between locked and unlocked Co-authored-by: David Swatzell <thatsstrikethree@gmail.com> Co-authored-by: Laurel Bonal <laurelbonal@gmail.com> Co-authored-by: Brandon Doza <brandonjamesdoza@gmail.com> Co-authored-by: Zach Wolek <zach.wolek@gmail.com>

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var newPalette = document.querySelector(".new-palette-button");
+var lock1 = document.querySelector('#lock1');
+var lock2 = document.querySelector('#lock2');
+// var lock3 = document.querySelector('#lock3');
+// var lock4 = document.querySelector('#lock4');
+// var lock5 = document.querySelector('#lock5');
+
 var boxes = [
-    {box: document.querySelector('#box1'), label: document.querySelector('#label1')},
-    {box: document.querySelector('#box2'), label: document.querySelector('#label2')},
-    {box: document.querySelector('#box3'), label: document.querySelector('#label3')},
-    {box: document.querySelector('#box4'), label: document.querySelector('#label4')},
-    {box: document.querySelector('#box5'), label: document.querySelector('#label5')}
+    {box: document.querySelector('#box1'), label: document.querySelector('#label1'), locked: false, lockIcon: document.querySelector('#lock1')},
+    {box: document.querySelector('#box2'), label: document.querySelector('#label2'), locked: false, lockIcon: document.querySelector('#lock2')},
+    {box: document.querySelector('#box3'), label: document.querySelector('#label3'), locked: false, lockIcon: document.querySelector('#lock3')},
+    {box: document.querySelector('#box4'), label: document.querySelector('#label4'), locked: false, lockIcon: document.querySelector('#lock4')},
+    {box: document.querySelector('#box5'), label: document.querySelector('#label5'), locked: false, lockIcon: document.querySelector('#lock5')}
 ];
 
 var currentPalette = [];
@@ -18,11 +24,61 @@ newPalette.addEventListener("click", function(){
 function showRandomColors() {
     currentPalette = [];
     boxes.forEach(function(item) {
+        if (!item.locked){
         var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         item.box.style.backgroundColor = randomColor;
         item.label.innerText = randomColor;
         currentPalette.push(randomColor);
+        }
     });
 };
 
+// function handleLock() { 
+//     boxes.forEach(function(item) {
+//         item.lockIcon.addEventListener("click", function() {
+//             toggleLock(item);
+//         });
+//     });
+// }
+    
+
+// function toggleLock(item) {
+//   item.locked = !item.locked
+//   if (item.locked){
+//     item.lockIcon.src = "assets/locked.png"
+//   }
+// }
+
+lock1.addEventListener('click', function() {
+  console.log('here');
+  handleLock(lock1);
+})
+
+lock2.addEventListener('click', function() {
+  console.log('here');
+  handleLock(lock2);
+})
+
+// var isLocked = false;
+
+// function handleLock(lock) {
+//   if (!isLocked) {
+//     lock.src="assets/locked.png";
+//     console.log('isLocked', isLocked)
+//     isLocked = true
+//     console.log(isLocked, 'true???')
+//   } else if (isLocked) {
+//     lock.src="assets/unlocked.png";
+//     isLocked = false
+//   }  
+// }
+
+function handleLock(lock) {
+  var isLocked = lock.src.includes("unlocked");
+  if (isLocked) {
+      lock.src = "assets/locked.png";
+  } else {
+      lock.src = "assets/unlocked.png";
+  }
+}
 
